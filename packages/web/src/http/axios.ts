9f754e13d6b7b2554a5863b05feff75bf0fd1b6e
AlexZzk <41469204+AlexZzk@users.noreply.github.com>
2026-03-11 10:27:18 +0800

    Update axios.ts

--- packages/web/src/http/axios.ts
+++ packages/web/src/http/axios.ts
@@ -1,21 +1,36 @@
 import axios from "axios";
 
 const service = axios.create({
-    baseURL: import.meta.env.VITE_BASE_API || '/', // 所有的请求地址前缀部分
-    timeout: 60000, // 请求超时时间毫秒
-    withCredentials: true, // 异步请求携带cookie
-    headers: {
-        // 设置后端需要的传参类型
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest',
-    },
-})
+    baseURL: import.meta.env.VITE_APP_BASE_API, // API前缀
+    timeout: 60000,
+});
 
 // 请求拦截器
 service.interceptors.request.use(
-)
-service.interceptors.response.use(
-)
+    (config) => {
+        // 可以在这里加入 token
+        // const token = localStorage.getItem("token")
+        // if (token) config.headers.Authorization = `Bearer ${token}`
 
-export default service
+        console.log("请求地址:", config.baseURL + config.url);
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// 响应拦截器
+service.interceptors.response.use(
+    (response) => {
+        console.log(response)
+        return response;
+    },
+    (error) => {
+        console.error("请求错误:", error);
+        return Promise.reject(error);
+    }
+);
+
+export default service;
